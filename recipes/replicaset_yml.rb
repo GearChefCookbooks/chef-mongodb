cd5e1099d0e4d13e2833a1dc1745486b407a70ba
Gary Leong <gwleong@gmail.com>
2014-09-09 11:44:42 -0700

    added test ruby build so it won't try to use the embedded ruby in chef

--- recipes/replicaset_yml.rb
+++ recipes/replicaset_yml.rb
@@ -18,6 +18,29 @@
 #
 
 include_recipe "mongodb"
+
+bash "compile ruby" do
+  user "root"
+  code <<-EOH
+    apt-get -y update
+    apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev libcurl4-openssl-dev curl git-core python-software-properties
+    wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p385.tar.gz
+    tar -xvzf ruby-1.9.3-p385.tar.gz
+    cd ruby-1.9.3-p385/
+    ./configure
+    make
+    sudo make install
+    echo "gem: --no-ri --no-rdoc" >> ~/.gemrc
+    sudo gem install bundler
+  EOH
+    not_if "ls /usr/local/bin/ruby"
+end
+
+execute "apt-get-update" do
+  command "apt-get update"
+end
+
+
 include_recipe "mongodb::mongo_gem"
 
 #node.set['mongodb']['is_replicaset'] = true
