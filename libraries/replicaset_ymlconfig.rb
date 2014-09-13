876f34bd65aeace6ad33eefd22a0fcc3eda71bb5
Gary Leong <gwleong@gmail.com>
2014-09-13 10:37:16 -0700

    clean up troubleshooting block for library replicaset_yml

--- libraries/replicaset_ymlconfig.rb
+++ libraries/replicaset_ymlconfig.rb
@@ -65,13 +65,12 @@ class Chef::ResourceDefinitionList::ReplicaHelper
     #We want at least 1 node to create a replicaset or a basis for a replicaset
     members.empty? ? replicaset = false : replicaset = true
 
-    Chef::Log.info "hello"
-    puts members
+    Chef::Log.info "Troubleshooting node objects that were created for mongo rs"
     members.each do |member|
       fqdn = member["fqdn"]
-      puts fqdn
-      puts node['mongodb']['config']['port']
+      hostname = member["hostname"]
       port = member["mongodb"]["config"]["port"] 
+      Chef::Log.info "mongodb hostname: #{hostname}, fqdn: #{fqdn}, port: #{port}"
     end
         
     #return false,replica_name,members
