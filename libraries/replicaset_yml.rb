ea9d33ea644110b5b1e423a19dcd2f503e1accb1
Gary Leong <gwleong@gmail.com>
2014-09-18 11:42:19 -0700

    fixed exit problem

--- libraries/replicaset_yml.rb
+++ libraries/replicaset_yml.rb
@@ -19,46 +19,47 @@ class Chef::ResourceDefinitionList::ReplicaHelper
        instances = self.read_replica_yml(node)
     rescue
        Chef::Log.warn "Cannot retrieve info from #{node["mongodb"]["config"]["replica_file"]}"
-       exit 1
+       instances = nil
     end
 
     priority = 500
     members = []
-    instances.each do |name, instance|
-      priority -= 5
-      member = Chef::Node.new
-      member.name(name)
-      new_replica_name = instance["cluster"]+"_"+instance["instance"]
 
-      if replica_name.nil?
-        replica_name = new_replica_name
-      else
-        if replica_name != new_replica_name
-          Chef::Log.error "This name has a different cluster name than the prior node"
-          exit 1
+    unless instances.nil?
+      instances.each do |name, instance|
+        priority -= 5
+        member = Chef::Node.new
+        member.name(name)
+        new_replica_name = instance["cluster"]+"_"+instance["instance"]
+
+        if replica_name.nil?
+          replica_name = new_replica_name
+        else
+          if replica_name != new_replica_name
+            Chef::Log.error "This name has a different cluster name than the prior node"
+          end
         end
+
+        member.default['fqdn'] = instance["ipaddresses"]["private"]
+        member.default['ipaddress'] = instance["ipaddresses"]["private"]
+        member.default['hostname'] = name
+
+        mongodb_attributes = {
+          # here we could support a map of instances to custom replicaset options in the custom json
+          'port' => node['mongodb']['port'],
+          'config' => { 'port' => node['mongodb']['config']['port'] },
+          'replica_name' => replica_name,
+          'replica_arbiter_only' => false,
+          'replica_build_indexes' => true,
+          'replica_hidden' => false,
+          'replica_slave_delay' => 0,
+          'replica_priority' => priority,
+          'replica_tags' => {}, # to_hash is called on this
+          'replica_votes' => 1
+        }
+        member.default['mongodb'] = mongodb_attributes
+        members << member
       end
-
-      member.default['fqdn'] = instance["ipaddresses"]["private"]
-      member.default['ipaddress'] = instance["ipaddresses"]["private"]
-      member.default['hostname'] = name
-
-      mongodb_attributes = {
-        # here we could support a map of instances to custom replicaset options in the custom json
-        'port' => node['mongodb']['port'],
-        'config' => { 'port' => node['mongodb']['config']['port'] },
-        'replica_name' => replica_name,
-        'replica_arbiter_only' => false,
-        'replica_build_indexes' => true,
-        'replica_hidden' => false,
-        'replica_slave_delay' => 0,
-        'replica_priority' => priority,
-        'replica_tags' => {}, # to_hash is called on this
-        'replica_votes' => 1
-      }
-      member.default['mongodb'] = mongodb_attributes
-      members << member
-
     end
 
     #We want at least 1 node to create a replicaset or a basis for a replicaset
