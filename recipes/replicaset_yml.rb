8b4b8d034b8b89705c192328fed7ce37bd7dc516
Gary Leong <gwleong@gmail.com>
2014-10-14 14:36:28 -0700

    corrected licensed....

--- recipes/replicaset_yml.rb
+++ recipes/replicaset_yml.rb
@@ -1,8 +1,8 @@
 #
 # Cookbook Name:: mongodb
-# Recipe:: replicaset
+# Recipe:: replicaset_yml
 #
-# Copyright 2011, edelight GmbH
+# Copyright 2014, Gary Leong
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
