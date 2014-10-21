3596e33277629c8258572d78064dd03f2bc11889
Gary Leong <gwleong@gmail.com>
2014-10-21 16:25:01 -0700

    readded mongod with replica added back in

--- recipes/yml_configure_mongod_replica.rb
+++ recipes/yml_configure_mongod_replica.rb
@@ -25,39 +25,39 @@ service node[:mongodb][:init_service_name] do
   action [:enable, :start]
 end
 
-#unless node['mongodb']['is_shard']
-#
-#  Chef::Log.info "Configuring replicaset with mongo nodes specified in yml file ..."
-#
-#  #Making a libary call to read yml file and see if there are any members
-#  replicaset,replica_name,members = Chef::ResourceDefinitionList::ReplicaHelper.replicaset_members(node)
-#
-#  Chef::Log.info "Replicaset is #{replicaset} ... "
-#  Chef::Log.info "Replica name is #{replica_name} ... "
-#  Chef::Log.info "Replica members are #{members} ... "
-#
-#  if replicaset 
-#
-#      Chef::Log.info "mongod.conf changed for: #{node["hostname"]} == #{hostname}"
-#      template node[:mongodb][:dbconfig_file] do
-#        source "mongodb.simple.repl.conf.erb"
-#        mode 0644
-#        owner "root"
-#        group "root"
-#        variables(
-#          :replica_name => replica_name
-#        )
-#        notifies :restart, "service[#{node[:mongodb][:init_service_name]}]"
-#      end
-#
-#  else
-#
-#     Chef::Log.warn "***********************************"
-#     Chef::Log.warn "No nodes found for a replica set ..."
-#     Chef::Log.warn "***********************************"
-#
-#  end
-#
-#end
+unless node['mongodb']['is_shard']
+
+  Chef::Log.info "Configuring replicaset with mongo nodes specified in yml file ..."
+
+  #Making a libary call to read yml file and see if there are any members
+  replicaset,replica_name,members = Chef::ResourceDefinitionList::ReplicaHelper.replicaset_members(node)
+
+  Chef::Log.info "Replicaset is #{replicaset} ... "
+  Chef::Log.info "Replica name is #{replica_name} ... "
+  Chef::Log.info "Replica members are #{members} ... "
+
+  if replicaset 
+
+      Chef::Log.info "mongod.conf changed for: #{node["hostname"]} == #{hostname}"
+      template node[:mongodb][:dbconfig_file] do
+        source "mongodb.simple.repl.conf.erb"
+        mode 0644
+        owner "root"
+        group "root"
+        variables(
+          :replica_name => replica_name
+        )
+        notifies :restart, "service[#{node[:mongodb][:init_service_name]}]"
+      end
+
+  else
+
+     Chef::Log.warn "***********************************"
+     Chef::Log.warn "No nodes found for a replica set ..."
+     Chef::Log.warn "***********************************"
+
+  end
+
+end
 
 
